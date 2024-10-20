d69739467242826e72a262dcf3d5d4aefe139024
Govind Rai <136880169+govindrai28@users.noreply.github.com>
2024-10-20 02:59:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all JS code as newer version of JS
+// alert("hello"); //we are using node.js,not browser
+
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+
+console.log("Govind")
+
+let name = "Govind"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number range in memory address=> 2 to power 53
+// bigint bignumber in trading,reddit,use in FaceBook
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => not assign the value
+// symbol => Unique 
+
+
+// object
+
+console.log(typeof null);//null is typeof object asked in interview
+console.log(typeof undefined); //undefined
